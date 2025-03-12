03741059895225f38c3ec298839119c75a76f81f
paulasvam <anapaulacv2004@gmail.com>
2025-03-12 10:59:58 -0400

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,3 @@
+lista_estudiantes = ["jose villalobos", "Jose Gonzalez", "Jose Perozo", "Juan Hidalgo", "Genesis Paz", "Raquel Carruyo", "Paula Carruyo", "Alberto Mendez", "Hebert Rondon", "Alicia Marcano", "Gabriel Barrera", "Santiago Salas", "Andan Cubillan", "Angel Medina"]
+lista_estudiantes[9] = "Daniel Espitia"
+print(lista_estudiantes)
---
+++ ejericicio2.py
@@ -0,0 +1,6 @@
+diccionario = {"nombre":"Paula Carruyo", "edad":"21 años", "pais":"venezuela", "pelicula favorita":"trollhunters"}
+ 
+print(diccionario["nombre"])
+print(diccionario["edad"])
+print(diccionario["pais"])   
+print(diccionario["pelicula favorita"])
